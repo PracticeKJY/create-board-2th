82303c2e85e673c19e53fa290d5e1a3effb1b768
PracticeKJY <poiijtl1004@naver.com>
2023-05-03 16:28:58 +0900

    ✨ feat. Header 컴포넌트 생성
    - Header.jsx 생성
    - layout.js에 헤더컴포넌트 연결
    - logo 추가
    - Header 컴포넌트 css 추가

---
+++ app/@components/header/Header.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import boardLogo from "../../../public/boardLogo.svg"
+
+import styles from "./Header.module.css"
+
+const Header = () => {
+  return (
+    <header>
+      <div className={styles.navBar}>
+        <Link href="/">
+          <Image src={boardLogo} alt="로고" />
+        </Link>
+        <ul className={styles.navList}>
+          <HeaderList href={"/"} text={"Home"} />
+          <HeaderList href={"/list"} text={"목록"} />
+          <HeaderList href={"/write"} text={"글쓰기"} />
+        </ul>
+      </div>
+    </header>
+  )
+}
+
+export default Header
+
+const HeaderList = ({ href, text }) => {
+  return (
+    <li>
+      <Link href={href}>{text}</Link>
+    </li>
+  )
+}
