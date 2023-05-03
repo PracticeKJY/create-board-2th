d72a43b9e56f8a7632610e0d59f50eb82c422424
PracticeKJY <poiijtl1004@naver.com>
2023-05-03 15:04:11 +0900

    ✨ feat. mongoDB 설치 및 연결세팅완료
    - next.js 새 프로젝트 생성
    - default css 삭제
    - mongoDB 라이브러리 설치
    - util > db.js에 mongoDB연결 유틸함수 생성
    - app> page.js로 DB 통신 작동 확인

---
+++ util/db.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb"
+
+//이렇게 담아두면 한번만 실행해도 여러곳에서 DB정보를 불러올 수 있음
+const url =
+  "mongodb+srv://admin:12341234@codingapple.qs8kvrl.mongodb.net/board?retryWrites=true&w=majority"
+const options = { useNewUrlParser: true }
+let connectDB
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url, options).connect()
+  }
+  connectDB = global._mongo
+} else {
+  connectDB = new MongoClient(url, options).connect()
+}
+export { connectDB }
